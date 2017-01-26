f5aba93c4a521f5de2f28535adb4fa00d6023ddf
Stefan Cameron <stefan@stefcameron.com>
2017-01-25 17:39:39 -0800

    WIP: Add MongoDB, auth route, new index route, Client and AuthCode models -- needs testing!

---
+++ routes/authorize.js
@@ -0,0 +1,90 @@
+//--// Route: /authorize
+
+const debug = require('debug')('try-oauth2:routes/authorize');
+const _ = require('lodash');
+const uuid = require('node-uuid');
+const ClientModel = require('../lib/models/Client');
+const AuthCodeModel = require('../lib/models/AuthCode');
+
+router.get('/authorize', (req, res, next) => {
+  const responseType = req.query.response_type || undefined;
+  const clientId = req.query.client_id || undefined;
+  const redirectUri = req.query.redirect_uri || undefined;
+  const scope = req.query.scope || undefined;
+  const state = req.query.state || undefined;
+
+  if (!responseType) {
+    debug('INVALID: missing response_type');
+    res.status(400).send('bad request: missing response_type');
+    return;
+  }
+
+  if (responseType !== 'code') {
+    debug(`INVALID: unsupported response type: "${responseType}"`);
+    res.status(400).send('bad request: unsupported response_type');
+    return;
+  }
+
+  if (!clientId || !_.isString(clientId)) {
+    debug('INVALID: missing client_id');
+    res.status(400).send('bad request: missing client_id');
+    return;
+  }
+
+  if (!redirectUri || !_.isString(redirectUri) || redirectUri.indexOf('http') !== 0) {
+    // TODO: validate this is a properly-formatted URL, perhaps limit to 'https'
+    //  and other reasonable secure protocols (excluding 'ftps' for example...)
+    debug('INVALID: missing or invalid redirect_uri');
+    res.status(400).send('bad request: missing or invalid redirect_uri');
+    return;
+  }
+
+  ClientModel.findOne({clientId: clientId}, (err, client) => {
+    if (err) {
+      debug(`ERROR: failed to find client "${clientId}": ${err.message}`);
+      next(err);
+      return;
+    }
+
+    if (!client) {
+      debug(`ERROR: unknown client "${clientId}"`);
+      res.status(404).end();
+      return;
+    }
+
+    if (redirectUri.includes(client.domain) < 0) {
+      // TODO: MUST harden this check...
+      debug(`INVALID: redirect_uri "${redirectUri}" is not within client.domain "${client.domain}"`);
+      res.status(400).end();
+      return;
+    }
+
+    if (scope !== client.scope) {
+      // TODO: handle scope: ask user to allow level of access to their data...
+    }
+
+    // generate a unique auth code for this request
+    const authCode = new AuthCodeModel({
+      clientId,
+      userId: client.userId,
+      redirectUri
+    });
+
+    authCode.save((err) => {
+      if (err) {
+        debug(`ERROR: failed to generate auth code: ${err.message}`);
+        next(new Error('failed to generate authorization code'));
+      }
+
+      if (redirectUri) {
+        const authRedirectUri = `${redirectUri}?code=${authCode.code}`;
+        if (state) {
+          authRedirectUri += `&state=${state}`;
+        }
+        res.redirect(authRedirectUri);
+      } else {
+        res.json({state, code: authCode.code});
+      }
+    });
+  });
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,51 @@
-var express = require('express');
-var router = express.Router();
+//--// Route: /
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const debug = require('debug')('try-oauth2:routes/index');
+const _ = require('lodash');
+const express = require('express');
+const router = express.Router();
+const ClientModel = require('../lib/models/Client');
+
+// GET /: creates a new client based on 'client_name:String', 'user_id:String',
+//  and 'domain:String' query parameters
+router.get('/', (req, res, next) => {
+  const name = req.query.client_name || undefined;
+  const userId = req.query.user_id || undefined;
+  const domain = req.query.domain || undefined;
+
+  if (!name || !_.isString(name)) {
+    debug('INVALID: missing name');
+    res.status(400).send('bad request: missing name');
+    return;
+  }
+
+  if (!userId || !_.isString(userId)) {
+    debug('INVALID: missing user_id');
+    res.status(400).send('bad request: missing user_id');
+    return;
+  }
+
+  // limit domain to 'host[:port]' for now
+  // TODO: check proper format (NOT a URI, just a domain name), run through
+  //  blacklist, allow 'localhost' and '127.0.0.1' (any port) for local testing
+  if (!domain || !_.isString(domain) || /^\w+(?:\:\d{1-5})$/.test(domain)) {
+    debug('INVALID: missing redirect_uri');
+    res.status(400).send('bad request: missing domain');
+    return;
+  }
+
+  const client = new ClientModel({name, userId, domain});
+
+  client.save((err) => {
+    if (err) {
+      next(new Error('client name exists already'));
+    } else {
+      res.json(client);
+    }
+  });
+
+  // TODO: bring this back later?
+  // res.render('index', {title: 'Express'});
 });
 
 module.exports = router;
