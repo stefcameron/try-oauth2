f9dfb11bb26d0d8487fee60200416493bb47a64b
Stefan Cameron <stefan@stefcameron.com>
2017-01-30 11:36:51 -0800

    Add 'GET /client[?client_name]' route to list clients

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const ClientModel = require('../lib/models/Client');
+const libUtil = require('../lib/util');
 
 // GET /: creates a new client based on 'client_name:String', 'user_id:String',
 //  and 'domain:String' query parameters
@@ -39,6 +40,7 @@ router.get('/', (req, res, next) => {
 
     client.save((err) => {
       if (err) {
+        debug('ERROR: %o', err);
         next(new Error('client name exists already'));
       } else {
         debug('new client created: %o', client);
@@ -48,6 +50,36 @@ router.get('/', (req, res, next) => {
   } else {
     // show home page
     res.render('index', {title: 'try-oath2'});
+  }
+});
+
+// GET /client: get one client or list client names; optional parameters:
+//  'client_name:String'
+// TODO: this isn't proper REST... should be refactored later
+router.get('/client', (req, res, next) => {
+  if (_.size(req.query) <= 0) {
+    // list clients
+    ClientModel.find({name: /.+/}, 'name', (err, clients) => {
+      if (err) {
+        debug('ERROR: failed to list clients: %o', err);
+        res.status(500).send('failed to list clients');
+      } else {
+        res.json(_.map(clients, 'name'));
+      }
+    });
+  } else if (req.query.client_name && _.isString(req.query.client_name)) {
+    // get one client
+    ClientModel.find({name: req.query.client_name}, (err, clients) => {
+      if (err) {
+        debug('ERROR: failed to search for clients: %o', err);
+        res.status(500).end();
+      } else if (clients.length !== 1) {
+        debug(`client "${req.query.client_name}" not found`);
+        res.status(404).end();
+      } else {
+        res.json(libUtil.sanitizeModel(clients[0]));
+      }
+    });
   }
 });
 
