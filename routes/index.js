414af74fcb97db21650a89e329e13217a2bb8b11
Stefan Cameron <stefan@stefcameron.com>
2017-02-25 17:23:12 -0800

    Create clients at 'POST /client'

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,12 @@ const router = express.Router();
 // for this example, assume the following users exist
 const existingUsers = ['user1', 'user2', 'user3'];
 
-// GET /: creates a new client given these parameters:
+// GET /: render home page
+router.get('/', (req, res, next) => {
+  res.render('index', {title: 'try-oath2'});
+});
+
+// POST /client: creates a new client given these properties:
 // - clientName:String Unique client (application) name.
 // - userId:String ID of the user who owns the client/application (NOT the user
 //   granting this client access to their data upon authorization...)
@@ -20,59 +25,53 @@ const existingUsers = ['user1', 'user2', 'user3'];
 //   redirectUri specified during the authorization process must be within this domain.
 // - [description:String] Optional client/application description to display to
 //   the user during the authorization process.
-// TODO: this should really be a JSON-based POST request...
-router.get('/', (req, res, next) => {
-  if (_.size(req.query) > 0) {
-    const name = req.query.clientName || undefined;
-    const userId = req.query.userId || undefined;
-    const domain = req.query.domain || undefined;
-    let description = req.query.description || undefined;
+router.post('/client', (req, res, next) => {
+  const name = req.body.clientName || undefined;
+  const userId = req.body.userId || undefined;
+  const domain = req.body.domain || undefined;
+  let description = req.body.description || undefined;
 
-    if (!name || !_.isString(name)) {
-      debug('INVALID: missing name');
-      res.status(400).send('bad request: missing name');
-      return;
-    }
-
-    if (!userId || !_.isString(userId) || !existingUsers.includes(userId)) {
-      debug('INVALID: missing/invalid userId: %s', userId);
-      res.status(400).send('bad request: missing/invalid userId');
-      return;
-    }
-
-    // limit domain to 'host[:port]' for now
-    // TODO: check proper format (NOT a URI, just a domain name), run through
-    //  blacklist, allow 'localhost' and '127.0.0.1' for local testing
-    if (domain !== 'localhost' && domain !== '127.0.0.1' && !libUtil.verifyFQDN(domain)) {
-      debug('INVALID: missing or invalid domain');
-      res.status(400).send('bad request: missing or invalid domain');
-      return;
-    }
-
-    // make sure it's a string but ignore
-    if (!_.isString(description)) {
-      if (description === null || description === undefined) {
-        description = undefined;
-      } else {
-        description = sanitizeHtml('' + description); // cast to string and sanitize
-      }
-    }
-
-    const client = new ClientModel({name, userId, domain, description});
-
-    client.save((err) => {
-      if (err) {
-        debug('ERROR: failed to create client "%s": %s', name, err.message);
-        next(new Error('client name exists already'));
-      } else {
-        debug('new client "%s" created', client.name);
-        res.json(libUtil.sanitizeModel(client));
-      }
-    });
-  } else {
-    // show home page
-    res.render('index', {title: 'try-oath2'});
+  if (!name || !_.isString(name)) {
+    debug('INVALID: missing name');
+    res.status(400).send('bad request: missing name');
+    return;
   }
+
+  if (!userId || !_.isString(userId) || !existingUsers.includes(userId)) {
+    debug('INVALID: missing/invalid userId: %s', userId);
+    res.status(400).send('bad request: missing/invalid userId');
+    return;
+  }
+
+  // limit domain to 'host[:port]' for now
+  // TODO: check proper format (NOT a URI, just a domain name), run through
+  //  blacklist, allow 'localhost' and '127.0.0.1' for local testing
+  if (domain !== 'localhost' && domain !== '127.0.0.1' && !libUtil.verifyFQDN(domain)) {
+    debug('INVALID: missing or invalid domain');
+    res.status(400).send('bad request: missing or invalid domain');
+    return;
+  }
+
+  // make sure it's a string but ignore
+  if (!_.isString(description)) {
+    if (description === null || description === undefined) {
+      description = undefined;
+    } else {
+      description = sanitizeHtml('' + description); // cast to string and sanitize
+    }
+  }
+
+  const client = new ClientModel({name, userId, domain, description});
+
+  client.save((err) => {
+    if (err) {
+      debug('ERROR: failed to create client "%s": %s', name, err.message);
+      next(new Error('client name exists already'));
+    } else {
+      debug('new client "%s" created', client.name);
+      res.json(libUtil.sanitizeModel(client));
+    }
+  });
 });
 
 // GET /client: get one client or list client names; optional parameters:
